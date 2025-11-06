c22022b6e0b24aeaed5f2b77d5c4ba8dee7043be
Yead191 <yead191@gmail.com>
2025-11-06 17:10:27 +0600

    initial setup

--- src/app/(website)/(Home)/page.tsx
+++ src/app/(website)/(Home)/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <section className="flex h-screen justify-center items-center">
-      <h1 className="text-7xl font-semibold text-red-400 text-center ">Welcome to Hell & Love</h1>
+      <h1 className="text-7xl font-semibold text-red-400 text-center ">Welcome to Hell & Love <span className="text-[10px]">bdCalling 💖</span></h1>
     </section>
   );
 }
