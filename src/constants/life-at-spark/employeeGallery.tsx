1b92781d0dd37210f644a8d5b7feac76e1b46d2c
Yead191 <yead191@gmail.com>
2025-11-14 17:52:05 +0600

    modified demo data

--- src/constants/life-at-spark/employeeGallery.tsx
+++ src/constants/life-at-spark/employeeGallery.tsx
@@ -4,16 +4,23 @@ export type ImageItem =
 
 const DEFAULT_IMAGES: ImageItem[] = [
   {
+    src: "https://i.ibb.co.com/Gv2nbhXn/farzana-img.png",
+    alt: "Abstract art",
+    name: "Love BdCalling",
+    designation: "UI/UX Designer",
+  },
+
+  {
+    src: "https://i.ibb.co.com/j9kDvByp/a8b7cc78680e6c336e639b5c1b5b02da9582bd74.jpg",
+    alt: "Nadir",
+    name: "Nadir Hossain",
+    designation: "UI/UX Designer",
+  },
+  {
     src: "https://i.ibb.co.com/FqzSsdfj/580512431-4116207195308135-3270780407261229590-n.jpg",
     alt: "Abstract art",
     name: "Asadur Rahaman Yead",
     designation: "Jr Frontend Developer",
-  },
-  {
-    src: "https://images.unsplash.com/photo-1755331039789-7e5680e26e8f?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "Abstract art",
-    name: "John Doe",
-    designation: "Painter",
   },
   {
     src: "https://images.unsplash.com/photo-1755569309049-98410b94f66d?q=80&w=772&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -22,10 +29,16 @@ const DEFAULT_IMAGES: ImageItem[] = [
     designation: "Sculptor",
   },
   {
-    src: "https://images.unsplash.com/photo-1755497595318-7e5e3523854f?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "https://i.ibb.co.com/rfHkfHB3/517693226-2530034364005098-6571637773511387247-n.jpg",
+    alt: "Abstract art",
+    name: "Farjana Ahamed Shuchona",
+    designation: "UI/UX Designer",
+  },
+  {
+    src: "https://i.ibb.co.com/qG6vvtQ/Frame-2147238452.png",
     alt: "Digital artwork",
-    name: "Bob Johnson",
-    designation: "Graphic Designer",
+    name: "Saiful Islam Fahim",
+    designation: "LF Member & Backend Developer",
   },
   {
     src: "https://images.unsplash.com/photo-1755353985163-c2a0fe5ac3d8?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -34,22 +47,22 @@ const DEFAULT_IMAGES: ImageItem[] = [
     designation: "Visual Artist",
   },
   {
-    src: "https://images.unsplash.com/photo-1745965976680-d00be7dc0377?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "Geometric pattern",
+    src: "https://i.ibb.co.com/jBsYrqv/Frame-2147238448.png",
+    alt: "Rafsan ",
     name: "David Brown",
     designation: "Pattern Designer",
   },
   {
-    src: "https://images.unsplash.com/photo-1752588975228-21f44630bb3c?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "Textured surface",
-    name: "Eva Green",
-    designation: "Surface Artist",
+    src: "https://i.ibb.co.com/RTLhwdcY/Frame-2147238514-1.png",
+    alt: "Hasibul Hasan Shanto",
+    name: "Hasibul Hasan Shanto",
+    designation: "LF Member & Flutter Developer",
   },
   {
-    src: "https://pbs.twimg.com/media/Gyla7NnXMAAXSo_?format=jpg&name=large",
-    alt: "Social media image",
-    name: "Frank Miller",
-    designation: "Photographer",
+    src: "https://i.ibb.co.com/Hfhw6ggV/asad.png",
+    alt: "Asaduzzaman Mahfuz",
+    name: "Asaduzzaman Mahfuz",
+    designation: "Sr Ui/UX Designer",
   },
 ];
 export default DEFAULT_IMAGES;
