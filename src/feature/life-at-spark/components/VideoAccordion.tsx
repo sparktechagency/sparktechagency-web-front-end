0d7091a405aaca4eeb70fbe9a68050b689769f6b
Yead191 <yead191@gmail.com>
2025-11-17 17:39:51 +0600

    more optimized video

--- src/feature/life-at-spark/components/VideoAccordion.tsx
+++ src/feature/life-at-spark/components/VideoAccordion.tsx
@@ -1,10 +1,12 @@
 "use client";
+import Image from "next/image";
 import { useState, useEffect } from "react";
 
 type Item = {
   title: string;
   content: string;
   video: string;
+  thumbnail?: string;
 };
 
 const ITEMS: Item[] = [
@@ -14,6 +16,8 @@ const ITEMS: Item[] = [
       "Think of us as your development talent pool on tap! We find the perfect developers, from juniors to full-stack pros, who integrate smoothly into your team. We support them every step of the way, ",
     video:
       "https://res.cloudinary.com/ds1njqypu/video/upload/v1763377312/Video_Compressor___Reduce_Video_File_Size_Online_e3l3jn.mp4",
+    thumbnail:
+      "https://res.cloudinary.com/ds1njqypu/image/upload/v1763102922/WhatsApp_Image_2025-11-14_at_12.38.37_acdf6d94_otlbb4.jpg",
   },
   {
     title: "Annual Tour",
@@ -21,6 +25,8 @@ const ITEMS: Item[] = [
       "A refreshing escape from the routine! Our annual tour brings the entire team together to explore new places, celebrate achievements, and create unforgettable memories—strengthening the bond beyond the workplace.",
     video:
       "https://res.cloudinary.com/ds1njqypu/video/upload/v1763377312/Video_Compressor___Reduce_Video_File_Size_Online_e3l3jn.mp4",
+    thumbnail:
+      "https://res.cloudinary.com/ds1njqypu/image/upload/v1763102921/WhatsApp_Image_2025-11-14_at_12.38.37_03d1bccb_h4wnai.jpg",
   },
   {
     title: "Woman’s day calibration",
@@ -28,6 +34,7 @@ const ITEMS: Item[] = [
       "A heartfelt tribute to the incredible women of our organization. We celebrate their contributions, achievements, and strength with engaging activities, appreciation moments, and a vibrant atmosphere full of joy.",
     video:
       "https://res.cloudinary.com/ds1njqypu/video/upload/v1763377312/Video_Compressor___Reduce_Video_File_Size_Online_e3l3jn.mp4",
+    thumbnail: "https://i.ibb.co.com/gMkL0wpK/Microsoft-Teams-image-1.jpg",
   },
   {
     title: "Townhall Party",
@@ -35,6 +42,8 @@ const ITEMS: Item[] = [
       "A lively gathering where the whole team comes together to share updates, celebrate milestones, and enjoy a fun, collaborative environment. It’s a perfect mix of communication, recognition, and celebration.",
     video:
       "https://res.cloudinary.com/ds1njqypu/video/upload/v1763377312/Video_Compressor___Reduce_Video_File_Size_Online_e3l3jn.mp4",
+    thumbnail:
+      "https://i.ibb.co.com/dJ3KHF5j/554997310-122147916110886951-7860946218894689302-n.jpg",
   },
 ];
 export default function VideoAccordion() {
@@ -61,18 +70,16 @@ export default function VideoAccordion() {
 
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 mt-16">
         {/* Video */}
-        <div className="rounded-xl overflow-hidden shadow">
-          <video
+        <div className="rounded-xl overflow-hidden shadow lg:h-[405px] lg:w-full" >
+          <Image
             key={ITEMS[current]?.video}
-            autoPlay
-            loop
-            muted
-            playsInline
-            preload="metadata"
-            className="w-full h-full object-cover"
-          >
-            <source src={ITEMS[current].video} type="video/mp4" />
-          </video>
+            src={ITEMS[current]?.thumbnail || "/placeholder.jpg"}
+            alt={ITEMS[current]?.title || "Image"}
+            height={500}
+            width={900}
+            className="object-cover h-full w-full"
+            priority={true}
+          />
         </div>
 
         {/* Accordion */}
